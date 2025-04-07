37a79711e3c6c17bae09803439d8da80908e65d9
nhat191024 <richberchannel01@gmail.com>
2025-04-07 14:28:23 +0700

    feat: create Tour model and migration for tours table

---
+++ app/Models/Tour.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tour extends Model
+{
+    protected $fillable = [
+        'name',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function locations()
+    {
+        return $this->hasMany(Location::class);
+    }
+
+    public function tourItems()
+    {
+        return $this->hasMany(TourItem::class);
+    }
+}
