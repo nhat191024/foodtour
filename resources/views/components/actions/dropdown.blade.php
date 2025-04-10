325c65ce3b9eba247403cef5da746524a3aa7ef9
Catty <ducvu73@gmail.com>
2025-04-10 20:29:14 +0700

    fix: add cursor pointer to dropdown trigger for better UX

--- resources/views/components/actions/dropdown.blade.php
+++ resources/views/components/actions/dropdown.blade.php
@@ -14,7 +14,7 @@
 @endphp
 
 <div {{ $attributes->merge(['class' => 'dropdown ' . $alignmentClasses]) }}>
-    <div class="m-1 flex items-center" tabindex="0" role="button"> {{ $trigger }}</div>
+    <div class="m-1 flex items-center cursor-pointer" tabindex="0" role="button"> {{ $trigger }}</div>
     <ul class="dropdown-content menu bg-base-100 rounded-box z-1 {{ $width }} border p-2 shadow-sm dark:border-gray-700 dark:bg-gray-800" tabindex="0">
         {{ $content }}
     </ul>
