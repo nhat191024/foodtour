52e12c706dd2239c58a57d3173032f88478eaa32
Catty <ducvu73@gmail.com>
2025-04-10 20:29:24 +0700

    feat: add routes for disabling and retrieving new tour items

--- routes/client.php
+++ routes/client.php
@@ -10,4 +10,8 @@ Route::middleware('auth')->group(function () {
     Route::post('/tour/detail', [HomeController::class, 'getTourById'])
     ->name('tour.detail');
     Route::post('/tour/submit', [HomeController::class, 'tourSubmit'])->name('tour.submit');
+    Route::post('/tour-item/disable', [HomeController::class, 'disableTourItem'])
+    ->name('tour-item.disable');
+    Route::post('/tour-item/new', [HomeController::class, 'getNewTourItem'])
+    ->name('tour-item.new');
 });
