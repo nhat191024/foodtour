84166f0e331c8e2079d5c7319eaee3f9ce56daae
Catty <ducvu73@gmail.com>
2025-04-11 19:45:23 +0700

    feat: add additional fields to tours table for food type, time, and number of days

--- database/migrations/2025_04_07_072216_create_tours_table.php
+++ database/migrations/2025_04_07_072216_create_tours_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('tours', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('food_type');
+            $table->string('time');
+            $table->string('number_of_days');
             $table->unsignedBigInteger('user_id');
             $table->timestamps();
         });
