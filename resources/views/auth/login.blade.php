c908de5031bfdde55cfd0defc2114d34bc69d3f0
Catty <ducvu73@gmail.com>
2025-04-25 16:43:39 +0700

    feat: add Google login button to authentication form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,6 +5,15 @@
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
+        <div class="mb-4 text-center">
+            <a href="{{ route('login.google') }}" class="btn btn-block btn-soft">
+                <svg class="w-5 h-5 mr-2" viewBox="0 0 24 24" fill="currentColor">
+                    <path d="M12.545,10.239v3.821h5.445c-0.712,2.315-2.647,3.972-5.445,3.972c-3.332,0-6.033-2.701-6.033-6.032s2.701-6.032,6.033-6.032c1.498,0,2.866,0.549,3.921,1.453l2.814-2.814C17.503,2.988,15.139,2,12.545,2C7.021,2,2.543,6.477,2.543,12s4.478,10,10.002,10c8.396,0,10.249-7.85,9.426-11.748L12.545,10.239z"/>
+                </svg>
+                {{ __('Đăng nhập với Google') }}
+            </a>
+        </div>
+
         <!-- Email Address -->
         <div>
             <x-inputs.label for="email" :value="__('Email')" />
