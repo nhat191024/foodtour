4abe23bea01a0de4d2e3850970abff9e59f0c562
Catty <ducvu73@gmail.com>
2025-04-26 06:08:23 +0700

    feat: implement save and retrieval of calculation history with food and member details

--- app/Http/Controllers/TourCalculatorController.php
+++ app/Http/Controllers/TourCalculatorController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\CalculateHistory;
 use App\Models\FavoriteTourItem;
+use App\Models\Food;
+use App\Models\Member;
 use App\Models\Tour;
 use App\Models\TourItem;
 use App\Service\AIService;
@@ -15,12 +18,6 @@ use function Pest\Laravel\json;
 
 class TourCalculatorController extends Controller
 {
-
-    public function __construct()
-    {
-
-    }
-
     public function getAllTours()
     {
         $allTours = Tour::where('user_id', auth()->id())->get();
@@ -34,5 +31,48 @@ class TourCalculatorController extends Controller
             ->firstOrFail();
         $allTourItems = TourItem::where('tour_id', $tourId)->get();
         return response()->json(['status' => 'success', 'data' => $allTourItems]);
+    }
+
+    public function saveCalculation(Request $request)
+    {
+        $record = CalculateHistory::create([
+            'trip_name' => $request->input('tripName') ?? $request->input('tourName'),
+            'total_per_person' => $request->input('perPerson'),
+            'total' => $request->input('total'),
+            'user_id' => auth()->id(),
+            'tour_item_id' => $request->input('tourItemId'),
+        ]);
+
+        $foodItems = $request->input('foodItems');
+        $memberItems = $request->input('members');
+
+        foreach ($foodItems as $item) {
+            Food::create([
+                'name' => $item['name'],
+                'price' => $item['price'],
+                'calculate_history_id' => $record->id,
+            ]);
+        }
+
+        foreach ($memberItems as $item) {
+            Member::create([
+                'name' => $item['name'],
+                'calculate_history_id' => $record->id,
+            ]);
+        }
+
+        return response()->json(['status' => 'success', 'data' => 'ok']);
+    }
+
+    public function getCalculation()
+    {
+        $results = CalculateHistory::with('members', 'foods', 'tourItem')->where('user_id', auth()->id())->get();
+        return response()->json(['status' => 'success', 'data' => $results]);
+    }
+
+    public function deleteCalculation($id)
+    {
+        CalculateHistory::find($id)->delete();
+        return response()->json(['status' => 'success']);
     }
 }
