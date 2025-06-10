3a24d7b8f53591be9506f958a154c796ecdec22c
nhat191024 <richberchannel01@gmail.com>
2025-06-10 13:36:34 +0700

    feat: create HistoryFood model and migration for tracking food history

---
+++ database/migrations/2025_06_10_061547_create_history_food_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('history_food', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('history_item_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('description');
+            $table->string('address');
+            $table->string('food_type')->nullable();
+            $table->string('note')->nullable();
+            $table->string('latitude')->nullable();
+            $table->string('longitude')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('history_food');
+    }
+};
