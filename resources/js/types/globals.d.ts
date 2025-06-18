a966d131b8873cea5d83b4f9bbdc9820f69132a9
mlemingcapoo <ducvu73@gmail.com>
2025-06-18 16:39:16 +0700

    feat: extend Inertia.js PageProps to include flash message types

--- resources/js/types/globals.d.ts
+++ resources/js/types/globals.d.ts
@@ -3,3 +3,15 @@ import type { route as routeFn } from 'ziggy-js';
 declare global {
     const route: typeof routeFn;
 }
+
+
+declare module '@inertiajs/core' {
+  interface PageProps {
+    flash: {
+      success: string | null;
+      error: string | null;
+    };
+  }
+}
+
+export {};
