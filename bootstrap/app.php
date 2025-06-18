abcf1036209a352b9619bed702525df913c88bc5
mlemingcapoo <ducvu73@gmail.com>
2025-06-18 16:48:09 +0700

    feat: enhance exception handling for non-API requests and add custom error messages for TooManyRequests

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Inertia\Inertia;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -23,5 +26,27 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+
+        $exceptions->render(function (Throwable $e, $request) {
+            if (! $request->is('api/*') && ! app()->runningInConsole()) {
+                $status = $e instanceof HttpException ? $e->getStatusCode() : 500;
+
+                if (in_array($status, [401, 403, 404, 500, 503])) {
+                    return Inertia::render('error/Show', [
+                        'status' => $status,
+                    ])->toResponse($request)->setStatusCode($status);
+                }
+            }
+
+            return null;
+        });
+
+        $exceptions->renderable(function (TooManyRequestsHttpException $e, $request) {
+            if ($request->header('X-Inertia')) {
+                $retryAfter = $e->getHeaders()['Retry-After'] ?? 'vài';
+                $message = "Bạn tác quá nhanh. Vui lòng đợi {$retryAfter} giây rồi bấm lại.";
+                return back()->with('error', $message);
+            }
+        });
+
     })->create();
