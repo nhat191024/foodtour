6391c435c9f975bd0efeb33ac9900c86f4aac867
mlemingcapoo <ducvu73@gmail.com>
2025-06-18 16:50:08 +0700

    feat: create ClientAppLayout component for layout structure with breadcrumbs

---
+++ resources/js/layouts/ClientAppLayout.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import ClientAppLayout from '@/layouts/app/ClientAppSidebarLayout.vue';
+import type { BreadcrumbItemType } from '@/types';
+
+interface Props {
+    breadcrumbs?: BreadcrumbItemType[];
+}
+
+withDefaults(defineProps<Props>(), {
+    breadcrumbs: () => [],
+});
+</script>
+
+<template>
+    <ClientAppLayout :breadcrumbs="breadcrumbs">
+        <slot />
+    </ClientAppLayout>
+</template>
