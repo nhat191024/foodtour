aed76acdc1b8012614f8f25608b62073ca111a45
mlemingcapoo <ducvu73@gmail.com>
2025-06-18 16:48:28 +0700

    feat: add 'Go Back to Profile' navigation item and update dashboard link in AppSidebar

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -5,13 +5,18 @@ import NavUser from '@/components/NavUser.vue';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-vue-next';
+import { BookOpen, Folder, LayoutGrid, StepBack } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
 const mainNavItems: NavItem[] = [
     {
+        title: 'Go Back to Profile',
+        href: '/profile',
+        icon: StepBack,
+    },
+    {
         title: 'Dashboard',
-        href: '/dashboard',
+        href: '/profile/edit',
         icon: LayoutGrid,
     },
 ];
